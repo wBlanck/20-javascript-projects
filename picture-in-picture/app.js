95a077f3176d47895f71cd51bd4fb80e7d66ebea
wblanck - laptop <blanck.william@gmail.com>
2022-01-13 01:31:15 +0100

    finito

--- picture-in-picture/app.js
+++ picture-in-picture/app.js
@@ -1,1 +1,26 @@
-console.log("hello there");
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadeddata = () => {
+      videoElement.play();
+    };
+  } catch (error) {
+    console.log("ooops something went wrong dawg", error);
+  }
+}
+
+button.addEventListener("click", async () => {
+  // disable button
+  button.disabled = true;
+  //start picture in picture
+  await videoElement.requestPictureInPicture();
+  //reset button
+  button.disabled = false;
+});
+
+selectMediaStream();
